d4b57bbb57277ab007bbedb7e846f6ec515cbde5
14431 <1443133203@qq.com>
2023-07-09 17:00:10 +0800

    变更

--- dynamicSlicer/src/main/java/com/ibm/jdi/test/DynamicSlicer.java
+++ dynamicSlicer/src/main/java/com/ibm/jdi/test/DynamicSlicer.java
@@ -5,10 +5,10 @@ import com.nju.boot.graphs.pdg.PDG;
 import java.util.Set;
 
 public class DynamicSlicer {
-    public void ProgramExecute(String path, String fileName, String className, String input, Integer line, PDG cdg) throws Exception {
+    public Set<Integer> ProgramExecute(String path, String fileName, String className, String input, Integer line, PDG cdg) throws Exception {
         DynamicExecuter dynamicExecuter = new DynamicExecuter();
         dynamicExecuter.executeFile(path, fileName, className, input);
         dynamicExecuter.buildingDDG(cdg);
-        Set<Integer> sliced = dynamicExecuter.dynamicSlice(line);
+        return dynamicExecuter.dynamicSlice(line);
     }
 }
