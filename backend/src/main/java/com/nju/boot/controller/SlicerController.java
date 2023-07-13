d7e37c1bb5b3d5017727653f7faf9e3db19ce5f1
chi3bian <apollotr0109@gmail.com>
2023-07-13 16:46:23 +0800

    modified:   ../backend/src/main/java/com/nju/boot/controller/FilesController.java 	modified:   ../backend/src/main/java/com/nju/boot/controller/SlicerController.java 	modified:   ../backend/src/main/java/com/nju/boot/service/impl/UserfileServiceImpl.java 	modified:   ../backend/src/main/resources/application.yml 	modified:   ../backend/target/classes/application.yml 	deleted:    ../staticSlicer/target/classes/classpath.index

--- backend/src/main/java/com/nju/boot/controller/SlicerController.java
+++ backend/src/main/java/com/nju/boot/controller/SlicerController.java
@@ -1,5 +1,8 @@
 package com.nju.boot.controller;
 
+import com.nju.boot.slicer.AbstractSlicer;
+import com.nju.boot.slicer.DataFlowEquationSlicer;
+import com.nju.boot.slicer.PDGSlicer;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Value;
@@ -18,14 +21,21 @@ public class SlicerController {
 
     @GetMapping("/dataflow")
     @ApiOperation(value = "基于数据流方程的切片")
-    public String DataFlowEquationSlicing(String id, int lineNumber, String variable){
-        return "";
+    public String DataFlowEquationSlicing(String id, int lineNumber, String variable) throws Exception{
+        if(lineNumber == 0 || variable.equals("")) {
+            throw new Exception("切片参数不齐全");
+        }
+        String path = fileUploadPath + "/" + id + ".java";
+        AbstractSlicer abstractSlicer = new DataFlowEquationSlicer(path);
+        return abstractSlicer.slice(lineNumber,variable).getResultCode();
     }
 
     @GetMapping("/pdg")
     @ApiOperation(value = "基于程序依赖图的切片")
     public String PDGSlicing(String id, int lineNumber, String variable){
-        return "";
+        String path = fileUploadPath + "/" + id + ".java";
+        AbstractSlicer abstractSlicer = new PDGSlicer(path);
+        return abstractSlicer.slice(lineNumber,variable).getResultCode();
     }
 
     @GetMapping("/dynamic")
