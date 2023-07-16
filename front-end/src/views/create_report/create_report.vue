05d65bb2efcf306e752ec37e9c4c6bc06e180ead
Surprise-Jing <jing_ya81@163.com>
2023-07-16 10:40:29 +0800

    显示图片

--- front-end/src/views/create_report/create_report.vue
+++ front-end/src/views/create_report/create_report.vue
@@ -45,27 +45,27 @@
         <h2>一.抽象语法树(AST,Abstract Syntax Tree)</h2>
         <div style="height: 400px;"></div>
         <div style="position: relative;">
-          <img src="../../assets/report.png" style="width: 700px;position:absolute; top:50%; left:50%; transform:translate(-50%,-50%);">
+          <img v-bind:src="ast" style="width: 700px;position:absolute; top:50%; left:50%; transform:translate(-50%,-50%);">
         </div>
         <div style="height: 1000px;"></div>
 
         <h2>二.控制流图(CFG,Control Flow Graph)</h2>
         <div style="height: 400px;"></div>
         <div style="position: relative;">
-          <img src="../../assets/report.png" style="width: 700px;position:absolute; top:50%; left:50%; transform:translate(-50%,-50%);">
+          <img v-bind:src="cfg" style="width: 700px;position:absolute; top:50%; left:50%; transform:translate(-50%,-50%);">
         </div>
         
         <div style="height: 900px;"></div>
         <h2>三.程序依赖图(PDG,Program Dependency Graph)</h2>
         <div style="height: 400px;"></div>
         <div style="position: relative;">
-          <img src="../../assets/report.png" style="width: 700px;position:absolute; top:50%; left:50%; transform:translate(-50%,-50%);">
+          <img v-bind:src="pdg" style="width: 700px;position:absolute; top:50%; left:50%; transform:translate(-50%,-50%);">
         </div>
         <div style="height: 800px;"></div>
         <h2>四.调用关系图(CG,Call Graph)</h2>
         <div style="height: 400px;"></div>
         <div style="position: relative;">
-          <img src="../../assets/report.png" style="width: 700px;position:absolute; top:50%; left:50%; transform:translate(-50%,-50%);">
+          <img v-bind:src="cg" style="width: 700px;position:absolute; top:50%; left:50%; transform:translate(-50%,-50%);">
         </div>
         <div style="height: 400px;"></div>
         <h2>五.代码度量(Code Metrics)</h2>
@@ -80,12 +80,12 @@
             <div style="height: 100px;"></div>
             <h3>调用分析</h3>
             <el-table :data="funcData"  style="width: 900px" v-if="refresh">
-                <el-table-column prop="funcname" label="TotalRow" width="150" />
-                <el-table-column prop="paranumber" label="CodeRow" width="150" />
-                <el-table-column prop="codedepth" label="Annotation" width="150"/>
-                <el-table-column prop="circlecomplexity" label="CircleComplexity" width="150"  />
-                <el-table-column prop="calltimes" label="CallTimes" width="150" />
-                <el-table-column prop="calledtimes" label="CalledTimes" />
+                <el-table-column prop="funcName" label="TotalRow" width="150" />
+                <el-table-column prop="param" label="CodeRow" width="150" />
+                <el-table-column prop="maxdepth" label="Annotation" width="150"/>
+                <el-table-column prop="cyclomatic" label="CircleComplexity" width="150"  />
+                <el-table-column prop="calling" label="CallTimes" width="150" />
+                <el-table-column prop="called" label="CalledTimes" />
             </el-table>
           </div>
 
@@ -103,6 +103,8 @@ import JSZip from'jszip'
 import FileSaver from'file-saver'
 import { saveAs } from 'file-saver';
 import { getFile } from '@/api/file'
+import {getInfoId} from '@/api/user'
+import {getLines, getCodeMetrics} from '@/api/metrics'
 import {AstPNG, CfgPNG, CgPNG, PdgPNG} from '@/api/create_report'
 import jsPDF from 'jspdf'
 import { blob } from 'd3'
@@ -125,10 +127,10 @@ export default {
 
       rowData:[
         {
-          totalrow:'1',
-          coderow:'2',
-          annotation:'3',
-          blankrow:'4'
+          // totalrow:'1',
+          // coderow:'2',
+          // annotation:'3',
+          // blankrow:'4'
         }
       ],
       funcData:[
@@ -171,8 +173,20 @@ export default {
             type:'warning',
             message: res.msg
           });
-        }}
-      )},
+        }}),
+        getInfoId(localStorage.getItem("uid")).then(res => {
+          if(res.success){
+          this.userName = res.data.username
+          console.log(this.userName);
+          //console.log(this.fileData)
+        }
+        else{
+          this.$message({
+            type:'warning',
+            message: res.msg
+          });
+        }})
+    },
     getfilecontext(val){
       // do nothing
         },
@@ -257,33 +271,111 @@ export default {
       },
 
       getData(){
-        
+        AstPNG(this.selectFile.id).then(res => {
+          if(res.success){
+              this.ast = require(res.data.result);
+              console.log(this.ast);
+             // this.$forceUpdate()
+            }
+            else{
+              this.$message({
+              type:'warning',
+              message: res.msg
+            })}
+        }),
 
+        CfgPNG(this.selectFile.id).then(res => {
+          if(res.success){
+              this.cfg = res.data.result;
+              console.log(this.cfg);
+             // this.$forceUpdate()
+            }
+            else{
+              this.$message({
+              type:'warning',
+              message: res.msg
+            })}
+        }),
+
+        CgPNG(this.selectFile.id).then(res => {
+          if(res.success){
+              this.cg = res.data.result;
+              console.log(this.cg);
+             // this.$forceUpdate()
+            }
+            else{
+              this.$message({
+              type:'warning',
+              message: res.msg
+            })}
+        }),
+
+        PdgPNG(this.selectFile.id).then(res => {
+          if(res.success){
+              this.pdg = res.data.result;
+              console.log(this.pdg);
+             // this.$forceUpdate()
+            }
+            else{
+              this.$message({
+              type:'warning',
+              message: res.msg
+            })}
+        }),
+
+        getLines(this.selectFile.id).then(res => {
+          if(res.success){
+            this.rowData.push({
+              totalraw:res.data.linesOfAll,
+              coderow:res.data.linesOfCode,
+              annotation:res.data.linesOfComment,
+              blankrow:res.data.linesOfBlanks
+            })
+              console.log(this.rowData);
+            }
+            else{
+              this.$message({
+              type:'warning',
+              message: res.msg
+            })}
+        }),
+
+        getCodeMetrics(this.selectFile.id).then(res => {
+          if(res.success){
+            this.funcData = res.data
+              console.log(this.funcData);
+            }
+            else{
+              this.$message({
+              type:'warning',
+              message: res.msg
+            })}
+        })
 
 
         //代码度量
-        this.rowData.push({
-          totalraw:'20',
-          coderow:'12',
-          annotation:'5',
-          blankrow:'3'
-        })
-        this.funcData.push({
-          "funcname":'getNum',
-          "paranumber":'2',
-          "codedepth":'3',
-          "circlecomplexity":'1',
-          "calltimes":'5',
-          "calledtimes":'7'
-        })
-        this.funcData.push({
-          funcname:'getRow',
-          paranumber:'1',
-          codedepth:'2',
-          circlecomplexity:'7',
-          calltimes:'4',
-          calledtimes:'8'
-        })
+        // this.rowData.push({
+        //   totalraw:'20',
+        //   coderow:'12',
+        //   annotation:'5',
+        //   blankrow:'3'
+        // })
+        // this.funcData.push({
+        //   "funcname":'getNum',
+        //   "paranumber":'2',
+        //   "codedepth":'3',
+        //   "circlecomplexity":'1',
+        //   "calltimes":'5',
+        //   "calledtimes":'7'
+        // })
+        // this.funcData.push({
+        //   funcname:'getRow',
+        //   paranumber:'1',
+        //   codedepth:'2',
+        //   circlecomplexity:'7',
+        //   calltimes:'4',
+        //   calledtimes:'8'
+        // })
         
       },
       refresh_table(){
