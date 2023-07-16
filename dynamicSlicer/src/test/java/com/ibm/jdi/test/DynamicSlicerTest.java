b2eca2c686e312dadd63ca1751d03bd0fa4cdf10
14431 <1443133203@qq.com>
2023-07-16 17:30:00 +0800

    Changes

--- dynamicSlicer/src/test/java/com/ibm/jdi/test/DynamicSlicerTest.java
+++ dynamicSlicer/src/test/java/com/ibm/jdi/test/DynamicSlicerTest.java
@@ -9,13 +9,13 @@ import java.nio.file.Paths;
 class DynamicSlicerTest {
     String absolutePath = new File("").getAbsolutePath();
     String filePath = Paths.get(absolutePath,"data","test","src").toString();
-    String fileName = "Test8.java";
+    String fileName = "Test2.java";
     String wholePath = Paths.get(filePath,fileName).toString();
     @Test
     void programExecute() throws Exception {
 //        Graphs graphs = new Graphs(wholePath);
-        int lineNumber = 15;
-        String input = "3";
+        int lineNumber = 21;
+        String input = "2 1 1";
 //        CDG cdg = graphs.getCDG(GraphsUtil.findMethodByLineNumber(graphs.getCu(),lineNumber));
 //        System.out.println(cdg.toString());
         String result = new DynamicSlicer(filePath + "\\" + fileName).slice(input ,lineNumber).getSlicedCode();
